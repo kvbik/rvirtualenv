651d99fbcde23b1b0b2d59f1bbe0731f8233f628
Jakub Vysoky <jakub@borka.cz>
2010-02-18 16:30:35 +0100

    generate python dirs via distutils

--- rvirtualenv/generate.py
+++ rvirtualenv/generate.py
@@ -1,19 +1,44 @@
 
 import os
 from os import path
-from shutil import copy
+from shutil import copy, rmtree, copytree
+import sys
+from distutils.core import setup
 
 import rvirtualenv
 
+
+def run_setup(what, where):
+    oldpath = os.getcwd()
+    oldprefix = sys.prefix
+    oldargv = sys.argv
+
+    sys.prefix = where
+    sys.argv = ['setup.py', 'install']
+    os.chdir(what)
+
+    dist = setup(
+        name='rvirtualenvkeep',
+        version='0.1',
+        py_modules=['rvirtualenvkeep'],
+        scripts=['bin/python.py'],
+    )
+
+    os.chdir(oldpath)
+    sys.prefix = oldprefix
+    sys.argv = oldargv
+
+    return dist
 
 def generate(where):
     '''
     create dirs and files after virtualenv dir itself is prepared
     '''
     base = path.dirname(rvirtualenv.__file__)
-    pybin = path.join(base, 'template', 'bin', 'python.py')
+    inst = path.join(base, 'template', 'inst')
+    tmp = path.join(where, 'tmp_inst')
 
-    os.chdir(where)
-    os.mkdir('bin')
-    copy(pybin, path.join(where, 'bin', 'python.py'))
+    copytree(inst, tmp)
+    run_setup(tmp, where)
+    rmtree(tmp)
 
