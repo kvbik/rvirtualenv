0bffd2bfee38a18643b027970166b7441fc4804e
Jakub Vysoky <jakub@borka.cz>
2010-02-19 14:26:29 -0500

    setup.py file

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name = 'RVirtualEnv',
+    description = "relocatable python virtual environment",
+    url = "http://github.com/kvbik/rvirtualenv",
+    version = '0.1.0',
+    author = "Jakub Vysoky",
+    author_email = "jakub@borka.cz",
+    license = "BSD",
+    packages = ['rvirtualenv'],
+    entry_points = {
+        'console_scripts': [
+            'rvirtualenv = rvirtualenv:main',
+        ],  
+    },
+    zip_safe = False,
+    include_package_data = True,
+    setup_requires = [
+        'setuptools_dummy',
+    ],  
+)   
+
