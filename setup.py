92539daba6472e3022b9dd3d96288d57b2373314
Jakub Vysoky <jakub@borka.cz>
2010-02-23 11:03:45 -0500

    use README.rst as a long_description

--- setup.py
+++ setup.py
@@ -1,15 +1,21 @@
 from setuptools import setup
+from os import path
 
 
 VERSION = (0, 2, 0)
 __version__ = VERSION
 __versionstr__ = '.'.join(map(str, VERSION))
 
+f = open(path.join(path.dirname(__file__), 'README.rst'))
+long_description = f.read().strip()
+f.close()
+
 
 setup(
     name = 'RVirtualEnv',
     description = "relocatable python virtual environment",
     url = "http://github.com/kvbik/rvirtualenv",
+    long_description = long_description,
     version = __versionstr__,
     author = "Jakub Vysoky",
     author_email = "jakub@borka.cz",
