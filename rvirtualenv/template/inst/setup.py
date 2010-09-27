1d1a2dbc9fa234651ede0ed1c36348a3e36eb266
Jakub Vysoky <jakub@borka.cz>
2010-09-27 11:36:51 +0200

    install activate as well

--- rvirtualenv/template/inst/setup.py
+++ rvirtualenv/template/inst/setup.py
@@ -1,11 +1,16 @@
 import sys
 from distutils.core import setup
 
-scripts = ['bin/python.py']
+scripts = [
+    'bin/python.py',
+    'bin/activate.py',
+]
 if sys.platform == 'win32':
     scripts.append('bin/python.bat')
 else:
     scripts.append('bin/python')
+    scripts.append('bin/activate')
+    scripts.append('bin/activate.template')
 
 setup(
     name='rvirtualenvkeep',
